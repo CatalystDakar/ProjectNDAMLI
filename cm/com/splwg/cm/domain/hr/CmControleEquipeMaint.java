7b58d3ea2efa1785febb9045ee67dd46e5e851e3
Denash <denashmoorthy@gmail.com>
2018-08-08 12:45:44 +0000

    Newly added

---
+++ cm/com/splwg/cm/domain/hr/CmControleEquipeMaint.java
@@ -0,0 +1,12 @@
+package com.splwg.cm.domain.hr;
+
+/**
+ * @author Divakar
+ *
+@EntityPageMaintenance (entity = cmControleEquipe, service = CONTM, program = CONTM,
+ *      body = @DataElement (contents = { @RowField (entity = cmControleEquipe, name = cmControleEquipe)}),
+ *      modules = { })
+ */
+public class CmControleEquipeMaint extends CmControleEquipeMaint_Gen {
+
+}
---
+++ cm/com/splwg/cm/domain/hr/CmControleEquipe_Impl.java
@@ -0,0 +1,10 @@
+package com.splwg.cm.domain.hr;
+
+/**
+ * @author Divakar
+ *
+@BusinessEntity (tableName = CM_CONTROLE_EQUIPE)
+ */
+public class CmControleEquipe_Impl extends CmControleEquipe_Gen {
+
+}
