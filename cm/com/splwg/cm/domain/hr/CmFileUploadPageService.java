5914fbf60252200431ca7cb861be361c68cada83
Denash <denashmoorthy@gmail.com>
2018-10-24 13:40:53 +0000

    New file for Upload

---
+++ cm/com/splwg/cm/domain/hr/CmFileUploadPageService.java
@@ -0,0 +1,14 @@
+package com.splwg.cm.domain.hr;
+
+/**
+ * @author Denash Kumar M
+ *
+@PageMaintenance (service = CMFILEUPLOAD, program = CMFILEUPLOAD,
+ *      body = @DataElement (contents = {@DataField (name = CM_FILEPATH)}),
+ *      modules = { "demo"},
+ *      actions = { "add"
+ *            , "change"})
+ */
+public class CmFileUploadPageService extends CmFileUploadPageService_Gen {
+
+}
